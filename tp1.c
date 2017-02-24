ad72caa479e47efbd8e100945adf20f3433069ee
NizarSemlali <nizarsemlali@hotmail.fr>
2017-02-24 17:38:56 -0500

    version de base : ouverture de fichier

--- tp1.c
+++ tp1.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <errno.h>
 
 int main ( int argc, char *argv[]) {
 
 
+    FILE* fichier = NULL;
 
-    printf("Version de base . ");
+    fichier = fopen("instructions.txt", "r");
+
+    if (fichier != NULL) {
+
+    	printf("fichier existe !\n");
+
+    } else {
+
+        fprintf(stderr, "%s\n", strerror(errno));
+
+    }
+
+
+    fclose(fichier);
     return 0 ; 
 
 
