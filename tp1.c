8d32afb150782430b1818710329c8e10ac94e11a
othmanebouaddi <othmanebouaddi@outlook.com>
2017-02-24 19:02:15 -0500

    Commit systemefichier

--- tp1.c
+++ tp1.c
@@ -8,20 +8,33 @@ int main ( int argc, char *argv[]) {
 
 
     FILE* fichier = NULL;
+    FILE* CD = NULL ; 
+
 
     fichier = fopen("instructions.txt", "r");
 
     if (fichier != NULL) {
 
     	printf("Le fichier existe !\n");
-        printf("Pas d'erreur ! \n") ; 
     } else {
 
         fprintf(stderr, "%s\n", strerror(errno));
 
     }
 
+    
+    CD = fopen("systemeFichier.txt", "w");
 
+    if ( CD != NULL ) {
+
+        printf("Le fichier existe !\n");
+        
+    } else {
+
+        fprintf(stderr, "%s\n", strerror(errno));
+
+    }
+    
     fclose(fichier);
     return 0 ; 
 
